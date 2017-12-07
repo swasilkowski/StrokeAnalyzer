a555648288777a724e6c4e9c060b82260e250d2c
swasilkowski <s.m.wasilkowski@gmail.com>
2017-12-07 13:26:52 +0100

    pdf generator added

--- StrokeAnalyzer/app/src/main/java/com/example/asus/strokeanalyzer/Model/Patient.java
+++ StrokeAnalyzer/app/src/main/java/com/example/asus/strokeanalyzer/Model/Patient.java
@@ -1,8 +1,19 @@
 package com.example.asus.strokeanalyzer.Model;
 
+import android.os.Environment;
+import android.util.Log;
+
 import com.example.asus.strokeanalyzer.Model.EnumValues.Region;
 import com.example.asus.strokeanalyzer.Model.Form.Answer.Answer;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Dictionary;
 import java.util.List;
 
@@ -32,4 +43,41 @@ public class Patient {
     public int PrognsisDragon;
 
     public void WriteToDatabse(){}
+
+    public void GenarateReport() {
+        Document doc = new Document();
+
+        try {
+            String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Dir";
+
+            File dir = new File(path);
+            if(!dir.exists())
+                dir.mkdirs();
+
+            File file = new File(dir, "newFile.pdf");
+            FileOutputStream fOut = new FileOutputStream(file);
+
+            PdfWriter.getInstance(doc, fOut);
+
+            //open the document
+            doc.open();
+
+            Paragraph p1 = new Paragraph("blalblabla");
+            Font paraFont= new Font(Font.FontFamily.COURIER);
+            p1.setAlignment(Paragraph.ALIGN_CENTER);
+            p1.setFont(paraFont);
+
+            //add paragraph to document
+            doc.add(p1);
+
+        } catch (DocumentException de) {
+            Log.e("PDFCreator", "DocumentException:" + de);
+        } catch (IOException e) {
+            Log.e("PDFCreator", "ioException:" + e);
+        }
+        finally {
+            doc.close();
+        }
+
+    }
 }
